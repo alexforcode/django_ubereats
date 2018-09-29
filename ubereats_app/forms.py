c73a2ff8b0e6c5b8138c04b20867bf6f66a4fa62
Alex Girin <alex.for.code@gmail.com>
2018-09-30 00:18:55 +0300

    Sign up forms.

---
+++ ubereats_app/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from ubereats_app.models import Restaurant
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+            'email')
+
+
+class RestaurantForm(forms.ModelForm):
+    class Meta:
+        model = Restaurant
+        fields = (
+            'name',
+            'phone',
+            'address',
+            'logo')
--- ubereats_app/views.py
+++ ubereats_app/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from ubereats_app.forms import RestaurantForm, UserForm
+
 
 def index(request):
     return redirect(restaurant_home)
 
 
 def restaurant_sign_up(request):
-    return render(request, 'restaurant/sign-up.html', {})
+    user_form = UserForm()
+    restaurant_form = RestaurantForm()
+
+    return render(request,
+                  'restaurant/sign-up.html',
+                  {'user_form': user_form,
+                   'restaurant_form': restaurant_form})
 
 
 @login_required(login_url='/restaurant/sign-in/')
