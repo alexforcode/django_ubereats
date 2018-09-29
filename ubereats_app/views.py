530f53a83c7ef85fe41080cac467879aecdbe485
Alex Girin <alex.for.code@gmail.com>
2018-09-29 23:38:36 +0300

    Sign up function.

--- ubereats_app/views.py
+++ ubereats_app/views.py
@@ -6,6 +6,10 @@ def index(request):
     return redirect(restaurant_home)
 
 
+def restaurant_sign_up(request):
+    return render(request, 'restaurant/sign-up.html', {})
+
+
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
     return render(request, 'restaurant/home.html', {})
