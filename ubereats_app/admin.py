5bdcfd1f7c526c7d07f93f558d6da1a659f3c93d
Alex Girin <alex.for.code@gmail.com>
2018-10-03 09:50:30 +0300

    Add customer and driver models.

--- ubereats_app/admin.py
+++ ubereats_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from ubereats_app.models import Restaurant
+from ubereats_app.models import Restaurant, Customer, Driver
 
 
 admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Driver)
