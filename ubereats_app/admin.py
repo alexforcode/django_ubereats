dd68544dc2f4889b2cad07bca797c607344c93dc
Alex Girin <alex.for.code@gmail.com>
2018-10-09 14:02:26 +0300

    Add order and order_details models.

--- ubereats_app/admin.py
+++ ubereats_app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from ubereats_app.models import Restaurant, Customer, Driver, Meal
+from ubereats_app.models import Restaurant, Customer, Driver, Meal, Order, OrderDetails
 
 
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(Driver)
 admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
